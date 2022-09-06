4fc6f3ba035961c6aa943d11df8822db3e84889a
Valentyn Stratii <stratiyvalter@gmail.com>
2022-09-06 23:58:25 +0300

    fix books filter and register admin

--- library/book/urls.py
+++ library/book/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns=[
     path('books/',get_books_page,name='books'),
     path('books/<int:id>',get_book_details,name='book-details'),
+    path('books/filter',get_filtered_books,name='filter-books')
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -4,23 +4,36 @@ from .models import Book
 from django.contrib.auth.decorators import login_required
 @login_required
 def get_books_page(request):
-    if request.method=='POST':
-        name=request.POST.get('name')
-        author_full=request.POST.get('authors_list')
-        if name!='':
-            return render(request,'book/books.html',{'books':Book.objects.filter(name=name)})
-        elif author_full!='Choose author':
-            books=[]
-            for book in Book.objects.all():
-                for author in book.authors.all():
-                    if author.name +' '+ author.patronymic +' '+ author.surname == author_full:
-                        books.append(book)
-            return render(request,'book/books.html',{'books':books})
-    else:                
-        book_list=Book.objects.all()
-        return render(request,'book/books.html',{'books':book_list})
+    # if request.method=='POST':
+    #     name=request.POST.get('name')
+    #     author_full=request.POST.get('authors_list')
+    #     if name!='':
+    #         return render(request,'book/books.html',{'books':Book.objects.filter(name=name)})
+    #     elif author_full!='Choose author':
+    #         books=[]
+    #         for book in Book.objects.all():
+    #             for author in book.authors.all():
+    #                 if author.name +' '+ author.patronymic +' '+ author.surname == author_full:
+    #                     books.append(book)
+    #         return render(request,'book/books.html',{'books':books})               
+    book_list=Book.objects.all()
+    return render(request,'book/books.html',{'books':book_list})
 @login_required
 def get_book_details(request,id):
     book=Book.objects.get(pk=id)
     author=book.authors.all()[0]
     return render(request,'book/book_details.html',{'this_book':book,'this_book_author':author})
+
+@login_required
+def get_filtered_books(request):
+    name=request.GET.get('name')
+    author_full=request.GET.get('authors_list')
+    if name!='':
+        return render(request,'book/books.html',{'books':Book.objects.filter(name=name)})
+    elif author_full!='Choose author':
+        books=[]
+        for book in Book.objects.all():
+            for author in book.authors.all():
+                if author.name +' '+ author.patronymic +' '+ author.surname == author_full:
+                    books.append(book)
+    return render(request,'book/filtered.html',{'books':books})     
