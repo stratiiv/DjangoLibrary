2efb2729fc81512cf07010dd0e34072a7757be5f
Valentyn Stratii <stratiyvalter@gmail.com>
2022-09-07 15:24:49 +0300

    finish

--- library/book/urls.py
+++ library/book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns=[
     path('books/',get_books_page,name='books'),
-    path('books/<int:id>',get_book_details,name='book-details'),
-    path('books/filter',get_filtered_books,name='filter-books'),
-    path('books/add',get_add_book,name='add-book')
+    path('books/<int:id>/',get_book_details,name='book-details'),
+    path('books/filter/',get_filtered_books,name='filter-books'),
+    path('books/add/',get_add_book,name='add-book')
 ]
