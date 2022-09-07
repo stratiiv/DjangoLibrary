5446b55a7755d160f909641f0e05bf555844bad8
Valentyn Stratii <stratiyvalter@gmail.com>
2022-09-07 14:58:59 +0300

    added create book form

--- library/book/urls.py
+++ library/book/urls.py
@@ -3,5 +3,6 @@ from .views import *
 urlpatterns=[
     path('books/',get_books_page,name='books'),
     path('books/<int:id>',get_book_details,name='book-details'),
-    path('books/filter',get_filtered_books,name='filter-books')
+    path('books/filter',get_filtered_books,name='filter-books'),
+    path('books/add',get_add_book,name='add-book')
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -2,20 +2,9 @@
 from django.shortcuts import render,redirect
 from .models import Book
 from django.contrib.auth.decorators import login_required
+from .forms import AddBookForm
 @login_required
-def get_books_page(request):
-    # if request.method=='POST':
-    #     name=request.POST.get('name')
-    #     author_full=request.POST.get('authors_list')
-    #     if name!='':
-    #         return render(request,'book/books.html',{'books':Book.objects.filter(name=name)})
-    #     elif author_full!='Choose author':
-    #         books=[]
-    #         for book in Book.objects.all():
-    #             for author in book.authors.all():
-    #                 if author.name +' '+ author.patronymic +' '+ author.surname == author_full:
-    #                     books.append(book)
-    #         return render(request,'book/books.html',{'books':books})               
+def get_books_page(request):        
     book_list=Book.objects.all()
     return render(request,'book/books.html',{'books':book_list})
 @login_required
@@ -37,3 +26,18 @@ def get_filtered_books(request):
                 if author.name +' '+ author.patronymic +' '+ author.surname == author_full:
                     books.append(book)
     return render(request,'book/filtered.html',{'books':books})     
+@login_required
+def get_add_book(request):
+    if request.user.groups.all()[0].name == "Admin":
+        if request.method=='POST':
+            form=AddBookForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('books')
+        else:
+            form=AddBookForm()
+            return render(request,'book/add.html',{'form':form})
+    else:
+        return redirect('login')
+
+    
