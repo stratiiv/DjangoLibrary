70177721cea2236c6ca277b45f0d7fa08f4db614
Valentyn Stratii <stratiyvalter@gmail.com>
2023-01-05 16:50:11 +0200

    fix not null constraint

--- order/models.py
+++ order/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
-    end_at = models.DateTimeField(blank=True)
+    end_at = models.DateTimeField(blank=True,null=True)
     expected_end_at = models.DateTimeField(default=django.utils.timezone.now()+datetime.timedelta(weeks=2))
 
     def __repr__(self):
